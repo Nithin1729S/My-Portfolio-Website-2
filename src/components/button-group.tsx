c1856f8e4bff0cfa7fb44718e056100fac5e4c94
Nithin1729S <sureshnithin1729@gmail.com>
2025-08-09 00:10:46 +0530

    update

--- src/components/button-group.tsx
+++ src/components/button-group.tsx
@@ -32,7 +32,7 @@ export default function ButtonGroup() {
         variant="outline"
         className="group text-skeptic-700 hover:bg-slate-100 hover:text-skeptic-800"
       >
-        <Link href="https://drive.google.com/file/d/10pKFHlAcIBSv9UCCUrCZzabxHf_Q-WKs/view?usp=sharing" prefetch={false} target="_blank">
+        <Link href="https://drive.google.com/file/d/1nbn2hXoACc8_1p6PFE38Fx0K21tgUUQe/view?usp=sharing" prefetch={false} target="_blank">
           Resume
         </Link>
       </Button>
